ece7444784a4032828bc7706a3514c5f0e051d55
Shounak Dey <shounakdey@ymail.com>
2025-06-14 23:16:16 +0530

    Refactor, add custom commands & multi-user support
     - Refactor the codebase to integrate all the previous commits
     - Add custom commands to generate message for single character
     - Add multi-user support for sending scheduled messages
    
    Signed-off-by: Shounak Dey <shounakdey@ymail.com>

--- gi_notifier/config.py
+++ gi_notifier/config.py
@@ -1,10 +1,11 @@
 from logging import getLogger
 from logging.config import dictConfig
+from pathlib import Path
 
 BOTTOKEN = ""
-CHATIDEN = ""
 TIMEZONE = "Asia/Kolkata"
 HOURLIST = [iter for iter in range(24) if iter % 4 == 0]
+DATA_FILE = Path("bot_user_data.json")
 
 logrconf = {
     "version": 1,
